5d6dbd9688d2dee12be45f651e4ffbe8654f153b
Anthony Chivetta <anthony@chivetta.org>
2012-03-15 04:31:06 -0400

    run the run.sh script on deploy

--- util/achivett/deploy.sh
+++ util/achivett/deploy.sh
@@ -4,5 +4,8 @@ for server in pvfs{1,2,3}; do
 	scp skye_client $server:skyefs_deploy
 	scp util/createthr $server:skyefs_deploy/util/
 	scp util/achivett/* $server:skyefs_deploy/util/
+done
 
+for server in pvfs{1,2,3}; do
+	ssh $server ./skyefs_deploy/util/run.sh &
 done
