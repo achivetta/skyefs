e884591e679c3bbb9e0dab9c77c263ca91501b5b
Anthony Chivetta <anthony@chivetta.org>
2012-03-23 03:00:37 -0400

    Make deployment directory.

--- util/achivett/deploy.sh
+++ util/achivett/deploy.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 for server in pvfs{1,2,3}; do
+	ssh $server mkdir -p skyefs_deploy/util
 	scp skye_server $server:skyefs_deploy
 	scp skye_client $server:skyefs_deploy
 	scp util/createthr $server:skyefs_deploy/util/
