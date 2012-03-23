5e45b931686ee0da57ef40bfaa0031dcb5457655
Anthony Chivetta <anthony@chivetta.org>
2012-03-23 05:43:22 -0400

    Working marmot run.sh.

--- util/marmot/run.sh
+++ util/marmot/run.sh
@@ -3,8 +3,12 @@
 set -e
 #set -v
 
+clients=8
+
 hostname=$(hostname | cut -d '.' -f 1)
-rm ~/logs/*$hostname*
+rm -f ~/logs/*$hostname*
+
+export HOSTNAME=$hostname.eth1
 
 #set up server
 sudo killall -q pvfs2-server || true
@@ -19,9 +23,9 @@ sleep 5
 
 echo "$hostname: Starting sync for PVFS STARTUP"
 if /usr/testbed/bin/emulab-sync -m; then
-	/usr/testbed/bin/emulab-sync -i 31 -n pvfs
+	/usr/testbed/bin/emulab-sync -i 31 -n pvfs-$1
 else
-	/usr/testbed/bin/emulab-sync -n pvfs
+	/usr/testbed/bin/emulab-sync -n pvfs-$1
 fi
 echo "$hostname: Finished sync for PVFS STARTUP"
 
@@ -32,24 +36,24 @@ killall createthr || true
 ulimit -c 10000
 
 cd ~/skyefs
-./skye_server -f 'tcp://node-0.skyefs.testbed:3334/pvfs2-fs' > ~/logs/skye_server-$hostname.log 2>&1 &
+./skye_server -f 'tcp://node-0.skyefs.gigaplus:3334/pvfs2-fs' > ~/logs/skye_server-$hostname.log 2>&1 &
 disown %1
 
 sleep 10
 
 echo "$hostname: Started sync for skye_server STARTUP"
 if /usr/testbed/bin/emulab-sync -m; then
-	/usr/testbed/bin/emulab-sync -i 31 -n server
+	/usr/testbed/bin/emulab-sync -i 31 -n server-$1
 else
-	/usr/testbed/bin/emulab-sync -n server
+	/usr/testbed/bin/emulab-sync -n server-$1
 fi
 echo "$hostname: Finished sync for skye_server STARTUP"
 
-for i in `seq 1 8`; do
+for i in `seq 1 $clients`; do
 	fusermount -u /tmp/skye_mnt_$i || true
 	sudo rm -rf /tmp/skye_mnt_$i
 	mkdir -p /tmp/skye_mnt_$i
-	./skye_client -o pvfs=tcp://node-0.skyefs.testbed:3334/pvfs2-fs -f -s /tmp/skye_mnt_$i > ~/logs/skye_client-${hostname}_$i.log 2>&1 &
+	./skye_client -o pvfs=tcp://node-0.skyefs.gigaplus:3334/pvfs2-fs -f -s /tmp/skye_mnt_$i > ~/logs/skye_client-${hostname}_$i.log 2>&1 &
 	disown %1
 done
 
@@ -58,13 +62,13 @@ sleep 10
 echo "$hostname: Started sync for skye_client STARTUP"
 if /usr/testbed/bin/emulab-sync -m; then
 	mkdir /tmp/skye_mnt_$i/0
-	/usr/testbed/bin/emulab-sync -i 31 -n client
+	/usr/testbed/bin/emulab-sync -i 31 -n client-$1
 else
-	/usr/testbed/bin/emulab-sync -n client
+	/usr/testbed/bin/emulab-sync -n client-$1
 fi
 echo "$hostname: Finished sync for skye_client STARTUP"
 
-for i in `seq 1 8`; do
+for i in `seq 1 $clients`; do
 	./util/createthr /tmp/skye_mnt_$i/0 4000 > ~/logs/createthr-${hostname}_$i.out 2> ~/logs/createthr-${hostname}_$i.log &
 	disown %1
 done
