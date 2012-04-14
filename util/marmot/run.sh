45a1325de66bb52259853706c3d027f6e1135a1f
Anthony Chivetta <anthony@chivetta.org>
2012-04-14 02:08:51 -0400

    Latest marmot run.sh

--- util/marmot/run.sh
+++ util/marmot/run.sh
@@ -33,10 +33,12 @@ killall skye_client || true
 killall skye_server || true
 killall createthr || true
 
-ulimit -c 10000
+echo '/tmp/core-%e-%h-%t-%c.%p' | sudo tee /proc/sys/kernel/core_pattern
+ulimit -c unlimited
 
 cd ~/skyefs
 ./skye_server -f 'tcp://node-0.skyefs.gigaplus:3334/pvfs2-fs' > ~/logs/skye_server-$hostname.log 2>&1 &
+
 disown %1
 
 sleep 10
@@ -50,10 +52,8 @@ fi
 echo "$hostname: Finished sync for skye_server STARTUP"
 
 for i in `seq 1 $clients`; do
-	fusermount -u /tmp/skye_mnt_$i || true
-	sudo rm -rf /tmp/skye_mnt_$i
-	mkdir -p /tmp/skye_mnt_$i
-	./skye_client -o pvfs=tcp://node-0.skyefs.gigaplus:3334/pvfs2-fs -f -s /tmp/skye_mnt_$i > ~/logs/skye_client-${hostname}_$i.log 2>&1 &
+	mkdir -p /tmp/skye_mnt_$1_$i
+	./skye_client -o pvfs=tcp://node-0.skyefs.gigaplus:3334/pvfs2-fs -f -s /tmp/skye_mnt_$1_$i > ~/logs/skye_client-${hostname}_$i.log 2>&1 &
 	disown %1
 done
 
@@ -61,7 +61,7 @@ sleep 10
 
 echo "$hostname: Started sync for skye_client STARTUP"
 if /usr/testbed/bin/emulab-sync -m; then
-	mkdir /tmp/skye_mnt_1/0
+	mkdir /tmp/skye_mnt_$1_1/0
 	/usr/testbed/bin/emulab-sync -i 31 -n client-$1
 else
 	/usr/testbed/bin/emulab-sync -n client-$1
@@ -69,7 +69,7 @@ fi
 echo "$hostname: Finished sync for skye_client STARTUP"
 
 for i in `seq 1 $clients`; do
-	./util/createthr /tmp/skye_mnt_$i/0 4000 > ~/logs/createthr-${hostname}_$i.out 2> ~/logs/createthr-${hostname}_$i.log &
+	./util/createthr /tmp/skye_mnt_$1_$i/0 32000 > ~/logs/createthr-${hostname}_$i.out 2> ~/logs/createthr-${hostname}_$i.log &
 	disown %1
 done
 
